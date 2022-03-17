36d721269a4c2651ddedfa816321497d44d03c49
Jiyeong Ha <jiyeong.ha@nyu.edu>
2022-03-17 16:34:27 -0400

    plot_params function added

--- plot_1D_model_results.py
+++ plot_1D_model_results.py
@@ -4,7 +4,8 @@ import os
 import seaborn as sns
 from matplotlib import pyplot as plt
 import sfp_nsd_utils as utils
-import
+import pandas as pd
+from first_level_analysis import np_log_norm_pdf
 
 def _merge_fitting_output_df_to_subj_df(fitting_df, subj_df, merge_on=["subj","vroinames", "eccrois"]):
     merged_df = subj_df.merge(fitting_df, on=merge_on)
@@ -103,3 +104,43 @@ def merge_and_plot(subj_to_run, fitting_df, subj_df, merge_on_cols=["subj", "vro
                               save_file_name=save_file_name)
 
     return merged_df, grid
+
+
+def plot_parameter_dist(output_df, subj_to_run=None, to_subplot="vroinames", n_sp_low=4,
+                           legend_out=True, to_label="eccrois",
+                           dp_to_x_axis='params', dp_to_y_axis='value',
+                           x_axis_label="Parameters", y_axis_label="Value",
+                           legend_title="Eccentricity", labels=['~0.5°', '0.5-1°', '1-2°', '2-4°', '4+°'],
+                           save_fig=False, save_dir='/Users/jh7685/Dropbox/NYU/Projects/SF/MyResults/',
+                           save_file_name='.png'):
+    #new_output_df = output_df.query('eccrois == @cur_ecc')
+    new_output_df = pd.melt(output_df, id_vars=['subj', 'vroinames', 'eccrois'], value_vars=['amp', 'mode', 'sigma'], ignore_index=True).rename(columns={'variable': 'params'})
+    col_order = utils.sort_a_df_column(new_output_df[to_subplot])
+    grid = sns.FacetGrid(new_output_df,
+                         col=to_subplot,
+                         col_order=col_order,
+                         hue=to_label,
+                         palette=sns.color_palette("husl"),
+                         legend_out=legend_out,
+                         sharex=True, sharey=True)
+    grid.map(sns.lineplot, dp_to_x_axis, dp_to_y_axis, ci=68, marker='o', linestyle='', err_style='bars', hue_order=utils.sort_a_df_column(output_df[to_label]))
+    grid.set_axis_labels(x_axis_label, y_axis_label)
+    lgd = grid.fig.legend(title=legend_title, bbox_to_anchor=(1.05, 0.8), loc="upper left", labels=labels, fontsize=15)
+    # Put the legend out of the figure
+    # g.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    for subplot_title, ax in grid.axes_dict.items():
+        ax.set_title(f"{subplot_title.title()}")
+    grid.fig.subplots_adjust(top=0.8)  # adjust the Figure in rp
+    n_subj = len(output_df['subj'].unique())
+    grid.fig.suptitle(f'N = {n_subj}', fontsize=18, fontweight="bold")
+    if save_fig:
+        if not save_dir:
+            raise Exception("Output directory is not defined!")
+        fig_dir = os.path.join(save_dir + y_axis_label + '_vs_' + x_axis_label)
+        if not os.path.exists(fig_dir):
+            os.makedirs(fig_dir)
+        save_path = os.path.join(fig_dir, 'all_subj')
+        plt.savefig(save_path, bbox_extra_artists=(lgd,), bbox_inches='tight')
+        plt.show()
+
+    return grid
