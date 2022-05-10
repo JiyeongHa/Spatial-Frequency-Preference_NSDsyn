c90daa8e8c810f55985552199b7e16316cd11ca4
Jiyeong Ha <jiyeongha@yonsei.ac.kr>
2022-05-10 16:30:14 -0700

    normalize() created

--- two_dimensional_model.py
+++ two_dimensional_model.py
@@ -25,47 +25,6 @@ def break_down_phase(df):
 
     return df
 
-
-df_dir='/Volumes/derivatives/subj_dataframes/for_MATLAB'
-
-
-for sn in np.arange(1,2):
-    subj = utils.sub_number_to_string(sn)
-    df_path = '/Volumes/server/Projects/sfp_nsd/natural-scenes-dataset/derivatives/subj_dataframes'
-    df = utils.load_df(sn, df_dir=df_path,
-            df_name='stim_voxel_info_df.csv')
-
-    # creating a dict file
-    df['hemi'] = df['hemi'].replace(['lh','rh'], [1,2])
-    dv_to_group = ['freq_lvl', 'names_idx', 'voxel', 'hemi']
-    df = df.groupby(dv_to_group).mean().reset_index()
-    selected_cols = ['voxel',
-                     'avg_betas', 'hemi', 'visualrois', 'eccrois',
-                     'eccentricity', 'angle', 'size', 'names_idx',
-                     'w_r', 'w_a', 'freq_lvl', 'local_ori', 'local_sf']
-    df = df[selected_cols].copy()
-    # save the final output
-    df_save_name = "%s_%s" % (subj, "mean_df_across_phase.csv")
-    df_save_dir = f'{df_path}/for_MATLAB'
-
-    if not os.path.exists(df_save_dir):
-        os.makedirs(df_save_dir)
-    df_save_path = os.path.join(df_save_dir, df_save_name)
-    df.to_csv(df_save_path, index=False, header=True)
-    print(f'... {subj} dataframe saved.')
-
-
-subj = utils.sub_number_to_string(1)
-df_file_name = 'stim_voxel_info_df.csv'
-df_path = os.path.join(df_dir, subj + '_' + df_file_name)
-df = binning._load_and_copy_df(df_path=df_path, create_vroinames_col=False, selected_cols=["fixation_task_betas", "memory_task_betas", "fixation_task", "memory_task"], remove_cols=True)
-
-df = utils.load_df(1, df_dir=df_dir, df_name='mean_df_across_phase.csv')
-
-params =pd.DataFrame({'sigma': [2.2], 'amp': [0.12], 'intercept': [0.35],
-          'p_1': [0.06], 'p_2': [-0.03], 'p_3': [0.07], 'p_4': [0.005],
-          'A_1': [0.04], 'A_2': [-0.01], 'A_3': [0], 'A_4': [0]})
-
 class Forward():
     """ Define parameters used in forward model"""
     def __init__(self, params, params_idx, subj_df):
@@ -84,8 +43,8 @@ class Forward():
         self.subj_df = subj_df.copy()
         self.theta_l = self.subj_df['local_ori']
         self.theta_v = self.subj_df['angle']
-        self.r_v = self.subj_df['eccentricity']
-        self.w_l = self.subj_df['local_sf']
+        self.r_v = self.subj_df['eccentricity'] # voxel eccentricity (in degrees)
+        self.w_l = self.subj_df['local_sf'] # in cycles per degree
 
 
     def get_Av(self):
@@ -110,9 +69,16 @@ class Forward():
         """ Return predicted BOLD response in eccentricity (formula no. 5 in Broderick et al. (2022)) """
         Av = self.get_Av()
         Pv = self.get_Pv()
-        return Av*np.exp(-(np.log2(self.w_l)+np.log2(Pv))**2/(2*self.sigma**2))
+        return Av*np.exp(-(np.log2(self.w_l)-np.log2(Pv))**2/(2*self.sigma**2))
 
+def normalize(df, to_norm, group_by=["voxel"]):
+    """calculate L2 norm for each voxel """
 
+    if all(df.groupby(group_by).size() == 28) is False:
+        raise Exception('There are more than 28 conditions for one voxel!\n')
+    l2_norm = df.groupby(group_by)[to_norm].apply(lambda x: x / np.linalg.norm(x))
+
+    return l2_norm
 
 
 class TwoDimensionalAnalysis(df, model_params):
