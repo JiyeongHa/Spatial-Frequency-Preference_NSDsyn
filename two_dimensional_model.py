3e6f6bc1e0a44ea26757d71d1f1a1ff52e72f7b3
Jiyeong Ha <jiyeongha@yonsei.ac.kr>
2022-05-11 10:54:35 -0700

    beta_comp() plot function created

--- two_dimensional_model.py
+++ two_dimensional_model.py
@@ -19,7 +19,7 @@ from hessian import hessian
 import binning_eccen as binning
 
 
-def break_down_phase(df):
+def break_down_phase():
     dv_to_group = ['subj', 'freq_lvl', 'names_idx', 'voxel', 'hemi']
     df = df.groupby(dv_to_group).mean().reset_index()
 
@@ -69,7 +69,7 @@ class Forward():
         """ Return predicted BOLD response in eccentricity (formula no. 5 in Broderick et al. (2022)) """
         Av = self.get_Av()
         Pv = self.get_Pv()
-        return Av*np.exp(-(np.log2(self.w_l)-np.log2(Pv))**2/(2*self.sigma**2))
+        return Av*np.exp(-(np.log2(self.w_l)+np.log2(Pv))**2/(2*self.sigma**2))
 
 def normalize(df, to_norm, group_by=["voxel"]):
     """calculate L2 norm for each voxel """
@@ -81,12 +81,40 @@ def normalize(df, to_norm, group_by=["voxel"]):
     return l2_norm
 
 
-class TwoDimensionalAnalysis(df, model_params):
-    """Dataset for first level results"""
-    def __init__(self, ):
-        self.df = df.reset_index()
-        self.device = device
-        self.df_path = df_path
-        self.stimulus_class = df.stimulus_class.unique()
-
-    def forward(self, idx):
+def beta_comp(sn, df, to_subplot="vroinames", to_label="eccrois",
+              dp_to_x_axis='norm_betas', dp_to_y_axis='norm_pred',
+              x_axis_label='Measured Betas', y_axis_label="Model estimation",
+              legend_title="Eccentricity", labels=['~0.5°', '0.5-1°', '1-2°', '2-4°', '4+°'],
+              n_row=4, legend_out=True,
+              save_fig=False, save_dir='/Users/auna/Dropbox/NYU/Projects/SF/MyResults/',
+              save_file_name='model_pred.png'):
+    subj = utils.sub_number_to_string(sn)
+    cur_df = df.query('subj == @subj')
+    col_order = utils.sort_a_df_column(cur_df[to_subplot])
+    grid = sns.FacetGrid(cur_df,
+                         col=to_subplot,
+                         col_order=col_order,
+                         hue=to_label,
+                         palette=sns.color_palette("husl"),
+                         col_wrap=n_row,
+                         legend_out=legend_out,
+                         sharex=True, sharey=True)
+    g = grid.map(sns.scatterplot, dp_to_x_axis, dp_to_y_axis)
+    grid.set_axis_labels(x_axis_label, y_axis_label)
+    grid.fig.legend(title=legend_title, bbox_to_anchor=(1, 1), labels=labels, fontsize=15)
+    # Put the legend out of the figure
+    # g.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    for subplot_title, ax in grid.axes_dict.items():
+        ax.set_title(f"{subplot_title.title()}")
+    grid.fig.subplots_adjust(top=0.8)  # adjust the Figure in rp
+    grid.fig.suptitle(f'{subj}', fontsize=18, fontweight="bold")
+    grid.tight_layout()
+    if save_fig:
+        if not save_dir:
+            raise Exception("Output directory is not defined!")
+        fig_dir = os.path.join(save_dir + y_axis_label + '_vs_' + x_axis_label)
+        if not os.path.exists(fig_dir):
+            os.makedirs(fig_dir)
+        save_path = os.path.join(fig_dir, f'{sn}_{save_file_name}')
+        plt.savefig(save_path)
+    plt.show()
