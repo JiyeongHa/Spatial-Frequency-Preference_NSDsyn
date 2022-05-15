591304a50bbd15daafad30e60ff73edc8d31df7f
Jiyeong Ha <jiyeong.ha@nyu.edu>
2022-05-15 17:03:57 -0400

    drop_voxels_outside_stim_range() created

--- voxel_selection.py
+++ voxel_selection.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import h5py
 import pandas as pd
+from math import atan2, degrees
 
 
 def drop_voxels_with_mean_negative_amplitudes(df):
@@ -15,6 +16,23 @@ def drop_voxels_with_mean_negative_amplitudes(df):
     df = df.query('voxel in @voxels')
     return df
 
+def pix_to_deg(size_in_pix=100, screen_height=39.29, n_pixel_height=1080, visual_distance=176.5):
+    """1920x1080 pixels, 69.84x39.29cm, 176.5 cm distance"""
+    theta = degrees(atan2(0.5*screen_height, visual_distance))
+    deg_per_pix = theta / (0.5*n_pixel_height)
+
+    return deg_per_pix * size_in_pix
+
+def drop_voxels_outside_stim_range(df, dv_to_group=['freq_lvl', 'subj']):
+    inner_radi = pix_to_deg(np.asarray([1.581139, 3.535534, 6.670832, 12.349089, 23.119256, 42.877733])) # in pixels
+    outer_radi = pix_to_deg(714/2)
+    df = df.query('@inner_radi[-1] < eccentricity < @outer_radi')
+    return df
+
+
+
+
+
 def _rgb2gray(rgb_image):
         return np.dot(rgb_image[..., :3], [0.2989, 0.5870, 0.1140])
 
@@ -26,8 +44,6 @@ def _get_distance(size=(714, 1360)):
     x, y = np.meshgrid(np.arange(1, size[1]+1)-origin[1],
                        np.arange(1, size[0]+1)-origin[0])
     return np.sqrt(x ** 2 + y ** 2)
-
-
 
 # first function: min & max eccentricity of stimulus
 def _find_ecc_range_of_stim(image='nsdsynthetic_stimuli.hdf5',
@@ -46,10 +62,12 @@ def _find_ecc_range_of_stim(image='nsdsynthetic_stimuli.hdf5',
         x, y = np.where(f[k,:,:] != mid_val)
         img_min_max[k, :] = [R[x, y].min(), R[x, y].max()]
 
-    return imag_min_max
+    return img_min_max
 
-def _find_min_and_max(img_min_max, stim_df=):
+def _find_min_and_max(img_min_max):
     """ find minimum val of max eccentricity & max val of minimum ecc"""
+    spiral_radii=[1.581139, 3.535534, 6.670832, 12.349089, 23.119256, 42.877733]
+
     stim_info_df = make_df._load_stim_info(stim_description_dir='/Users/jh7685/Dropbox/NYU/Projects/SF/natural-scenes-dataset/derivatives')
     img_df = pd.DataFrame(img_min_max).rename(columns={0: 'min_stim_ecc_R', 1: 'max_stim_ecc_R'})
     img_df = img_df.reset_index()
@@ -57,19 +75,3 @@ def _find_min_and_max(img_min_max, stim_df=):
     outer_boundary = img_min_max[:,1].min()
     inner_boundary = img_min_max[:,0]
     return inner_boundary, outer_boundary
-
-# each voxel's ecc -> # return voxels that have prf centers outside of stimuli
-def _check_voxel_ecc_range(min_ecc=min_ecc, max_ecc=max_ecc):
-    pass
-    return voxel_df
-
-def
-
-# remove the voxels from dataframe
-
-def drop_voxels_outside_ecc_range(df):
-    groupby_col = ['voxel']
-    if 'indicator' in df.columns:
-        groupby_col += ['indicator']
-    df = df.groupby(groupby_col).filter(lambda x: (x.eccen + x.sigma <= outer_border).all())
-    df = df.groupby(groupby_col).filter(lambda x: (x.eccen - x.sigma >= inner_border).all())
