86513970af7b9ae5880f907005b43e8718ea92e4
Jiyeong Ha <jiyeong.ha@nyu.edu>
2022-05-19 14:04:23 -0400

    check_28cond() created

--- two_dimensional_model.py
+++ two_dimensional_model.py
@@ -30,7 +30,7 @@ class Forward():
     def __init__(self, params, params_idx, subj_df):
         self.params_df = params.iloc[params_idx]
         self.sigma = self.params_df['sigma']
-        self.amp = self.params_df['amp']
+        self.amp = self.params_df['slope']
         self.intercept = self.params_df['intercept']
         self.p_1 = self.params_df['p_1']
         self.p_2 = self.params_df['p_2']
@@ -90,9 +90,9 @@ def beta_comp(sn, df, to_subplot="vroinames", to_label="eccrois",
               save_file_name='model_pred.png'):
     subj = utils.sub_number_to_string(sn)
     cur_df = df.query('subj == @subj')
-    #col_order = utils.sort_a_df_column(cur_df[to_subplot])
+    col_order = utils.sort_a_df_column(cur_df[to_subplot])
     grid = sns.FacetGrid(cur_df,
-                         col=to_subplot, #col_order=col_order,
+                         col=to_subplot, col_order=col_order,
                          hue=to_label,
                          palette=sns.color_palette("husl"),
                          col_wrap=n_row,
@@ -100,18 +100,20 @@ def beta_comp(sn, df, to_subplot="vroinames", to_label="eccrois",
                          sharex=True, sharey=True)
     if set_max:
         max_point = cur_df[[dp_to_x_axis, dp_to_y_axis]].max().max()
-        grid.set(xlim=(0, max_point), ylim=(0, max_point))
+        grid.set(xlim=(0, max_point+0.025), ylim=(0, max_point+0.025))
+
     g = grid.map(sns.scatterplot, dp_to_x_axis, dp_to_y_axis, alpha=alpha)
     grid.set_axis_labels(x_axis_label, y_axis_label)
-    #grid.fig.legend(title=legend_title, bbox_to_anchor=(1, 1),
-    #                labels=labels, fontsize=15)
+    grid.fig.legend(title=legend_title, bbox_to_anchor=(1, 1),
+                    labels=labels, fontsize=15)
     # Put the legend out of the figure
     # g.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     for subplot_title, ax in grid.axes_dict.items():
         ax.set_title(f"{subplot_title.title()}")
-        ax.set_ylim(ax.get_xlim())
-        ax.set_aspect('equal')
-    grid.fig.subplots_adjust(top=0.8)  # adjust the Figure in rp
+        #ax.set_xlim(ax.get_ylim())
+        #ax.set_xticks(ax.get_yticks())
+        ax.set_aspect('equal', adjustable='box')
+    grid.fig.subplots_adjust(top=0.8, right=0.9)  # adjust the Figure in rp
     grid.fig.suptitle(f'{subj}', fontsize=18, fontweight="bold")
     #grid.tight_layout()
     if save_fig:
@@ -123,6 +125,7 @@ def beta_comp(sn, df, to_subplot="vroinames", to_label="eccrois",
         save_path = os.path.join(fig_dir, f'{sn}_{save_file_name}')
         plt.savefig(save_path, bbox_inches='tight')
     plt.show()
+    return grid
 
 def beta_2Dhist(sn, df, to_subplot="vroinames", to_label="eccrois",
               dp_to_x_axis='norm_betas', dp_to_y_axis='norm_pred',
@@ -204,3 +207,86 @@ def beta_1Dhist(sn, df, to_subplot="vroinames",
         save_path = os.path.join(fig_dir, f'{sn}_{save_file_name}')
         plt.savefig(save_path)
     plt.show()
+
+# numpy to torch function
+def _cast_as_tensor(x):
+    """ Change numpy vector to torch vector. The input x should be either a column of dataframe,
+     a list, or numpy vector.You can also pass a torch vector but it will print out warnings."""
+    if type(x) == pd.Series:
+        x = x.values
+    # needs to be float32 to work with the Hessian calculations
+    return torch.tensor(x, dtype=torch.float32)
+
+def _cast_as_param(x, requires_grad=True):
+    """ Change input x to """
+    return torch.nn.Parameter(_cast_as_tensor(x), requires_grad=requires_grad)
+
+def _cast_args_as_tensors(args, on_cuda=False):
+
+    return_args = []
+    for v in args:
+        if not torch.is_tensor(v):
+            v = _cast_as_tensor(v)
+        if on_cuda:
+            v = v.cuda()
+        return_args.append(v)
+    return return_args
+
+# SF model class
+class SpatialFrequencyModel(torch.nn.Module):
+    def __init__(self):
+
+        super().__init__()
+
+        self.sigma = _cast_as_param(np.random.random(1))
+        self.slope = _cast_as_param(np.random.random(1))
+        self.intercept = _cast_as_param(np.random.random(1))
+        self.p_1 = _cast_as_param(np.random.random(1))
+        self.p_2 = _cast_as_param(np.random.random(1))
+        self.p_3 = _cast_as_param(np.random.random(1))
+        self.p_4 = _cast_as_param(np.random.random(1))
+        self.A_1 = _cast_as_param(np.random.random(1))
+        self.A_2 = _cast_as_param(np.random.random(1))
+        self.A_3 = 0
+        self.A_4 = 0
+
+
+    def get_Av(self, theta_l, theta_v):
+        """ Calculate A_v (formula no. 7 in Broderick et al. (2022)) """
+        theta_l = _cast_as_tensor(theta_l)
+        theta_v = _cast_as_tensor(theta_v)
+        Av = 1 + self.A_1 * torch.cos(2 * theta_l) + \
+             self.A_2 * torch.cos(4 * theta_l) + \
+             self.A_3 * torch.cos(2 * (theta_l - theta_v)) + \
+             self.A_4 * torch.cos(4 * (theta_l - theta_v))
+        return Av
+
+    def get_Pv(self, theta_l, theta_v, r_v):
+        """ Calculate p_v (formula no. 6 in Broderick et al. (2022)) """
+        theta_l = _cast_as_tensor(theta_l)
+        theta_v = _cast_as_tensor(theta_v)
+        r_v = _cast_as_tensor(r_v)
+        ecc_dependency = self.slope * r_v + self.intercept
+        Pv = ecc_dependency * (1 + self.A_1 * torch.cos(2 * theta_l) +
+                               self.A_2 * torch.cos(4 * theta_l) +
+                               self.A_3 * torch.cos(2 * (theta_l - theta_v)) +
+                               self.A_4 * torch.cos(4 * (theta_l - theta_v)))
+        return Pv
+
+    def forward(self, theta_l, theta_v, r_v, w_l):
+        """ Return predicted BOLD response in eccentricity (formula no. 5 in Broderick et al. (2022)) """
+        w_l = _cast_as_tensor(w_l)
+
+        Av = self.get_Av(theta_l, theta_v)
+        Pv = self.get_Pv(theta_l, theta_v, r_v)
+        return Av * torch.exp(-(torch.log2(w_l) + torch.log2(Pv)) ** 2 / (2 * self.sigma ** 2))
+
+
+    def objective_func(self, x, y):
+        mse_loss = torch.nn.functional.mse_loss(x, torch.from_numpy(y).to(torch.float32))
+
+        #Lv = (1/self.sigma**2)
+        return mse_loss
+
+def count_nan_in_torch_vector(x):
+    return torch.nonzero(torch.isnan(torch.log2(x).view(-1)))
