ede247e9de4ae7d88afd36701d25ec602bd6d0d1
Jiyeong Ha <jiyeong.ha@nyu.edu>
2022-05-22 16:58:46 -0400

    subdividing 2D model process

--- two_dimensional_model.py
+++ two_dimensional_model.py
@@ -1,5 +1,5 @@
 import sys
-#sys.path.append('../../')
+# sys.path.append('../../')
 import os
 import seaborn as sns
 import sfp_nsd_utils as utils
@@ -25,8 +25,10 @@ def break_down_phase(df):
 
     return df
 
+
 class Forward():
     """ Define parameters used in forward model"""
+
     def __init__(self, params, params_idx, subj_df):
         self.params_df = params.iloc[params_idx]
         self.sigma = self.params_df['sigma']
@@ -43,33 +45,33 @@ class Forward():
         self.subj_df = subj_df.copy()
         self.theta_l = self.subj_df['local_ori']
         self.theta_v = self.subj_df['angle']
-        self.r_v = self.subj_df['eccentricity'] # voxel eccentricity (in degrees)
-        self.w_l = self.subj_df['local_sf'] # in cycles per degree
-
+        self.r_v = self.subj_df['eccentricity']  # voxel eccentricity (in degrees)
+        self.w_l = self.subj_df['local_sf']  # in cycles per degree
 
     def get_Av(self):
         """ Calculate A_v (formula no. 7 in Broderick et al. (2022)) """
 
-        Av = 1 + self.A_1*np.cos(2*self.theta_l) + \
-             self.A_2*np.cos(4*self.theta_l) + \
-             self.A_3*np.cos(2*(self.theta_l - self.theta_v)) + \
-             self.A_4*np.cos(4*(self.theta_l-self.theta_v))
+        Av = 1 + self.A_1 * np.cos(2 * self.theta_l) + \
+             self.A_2 * np.cos(4 * self.theta_l) + \
+             self.A_3 * np.cos(2 * (self.theta_l - self.theta_v)) + \
+             self.A_4 * np.cos(4 * (self.theta_l - self.theta_v))
         return Av
 
     def get_Pv(self):
         """ Calculate p_v (formula no. 6 in Broderick et al. (2022)) """
-        ecc_dependency = self.amp*self.r_v + self.intercept
-        Pv = ecc_dependency*(1 + self.A_1*np.cos(2*self.theta_l) +
-                             self.A_2*np.cos(4*self.theta_l) +
-                             self.A_3*np.cos(2*(self.theta_l - self.theta_v)) +
-                             self.A_4*np.cos(4*(self.theta_l-self.theta_v)))
+        ecc_dependency = self.amp * self.r_v + self.intercept
+        Pv = ecc_dependency * (1 + self.A_1 * np.cos(2 * self.theta_l) +
+                               self.A_2 * np.cos(4 * self.theta_l) +
+                               self.A_3 * np.cos(2 * (self.theta_l - self.theta_v)) +
+                               self.A_4 * np.cos(4 * (self.theta_l - self.theta_v)))
         return Pv
 
     def two_dim_prediction(self):
         """ Return predicted BOLD response in eccentricity (formula no. 5 in Broderick et al. (2022)) """
         Av = self.get_Av()
         Pv = self.get_Pv()
-        return Av*np.exp(-(np.log2(self.w_l)+np.log2(Pv))**2/(2*self.sigma**2))
+        return Av * np.exp(-(np.log2(self.w_l) + np.log2(Pv)) ** 2 / (2 * self.sigma ** 2))
+
 
 def normalize(df, to_norm, group_by=["voxel"]):
     """calculate L2 norm for each voxel """
@@ -100,7 +102,7 @@ def beta_comp(sn, df, to_subplot="vroinames", to_label="eccrois",
                          sharex=True, sharey=True)
     if set_max:
         max_point = cur_df[[dp_to_x_axis, dp_to_y_axis]].max().max()
-        grid.set(xlim=(0, max_point+0.025), ylim=(0, max_point+0.025))
+        grid.set(xlim=(0, max_point + 0.025), ylim=(0, max_point + 0.025))
 
     g = grid.map(sns.scatterplot, dp_to_x_axis, dp_to_y_axis, alpha=alpha)
     grid.set_axis_labels(x_axis_label, y_axis_label)
@@ -110,12 +112,12 @@ def beta_comp(sn, df, to_subplot="vroinames", to_label="eccrois",
     # g.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     for subplot_title, ax in grid.axes_dict.items():
         ax.set_title(f"{subplot_title.title()}")
-        #ax.set_xlim(ax.get_ylim())
-        #ax.set_xticks(ax.get_yticks())
+        # ax.set_xlim(ax.get_ylim())
+        # ax.set_xticks(ax.get_yticks())
         ax.set_aspect('equal', adjustable='box')
     grid.fig.subplots_adjust(top=0.8, right=0.9)  # adjust the Figure in rp
     grid.fig.suptitle(f'{subj}', fontsize=18, fontweight="bold")
-    #grid.tight_layout()
+    # grid.tight_layout()
     if save_fig:
         if not save_dir:
             raise Exception("Output directory is not defined!")
@@ -127,13 +129,14 @@ def beta_comp(sn, df, to_subplot="vroinames", to_label="eccrois",
     plt.show()
     return grid
 
+
 def beta_2Dhist(sn, df, to_subplot="vroinames", to_label="eccrois",
-              dp_to_x_axis='norm_betas', dp_to_y_axis='norm_pred',
-              x_axis_label='Measured Betas', y_axis_label="Model estimation",
-              legend_title="Eccentricity", labels=['~0.5°', '0.5-1°', '1-2°', '2-4°', '4+°'],
-              n_row=4, legend_out=True, alpha=0.5, bins=30, set_max=False,
-              save_fig=False, save_dir='/Users/auna/Dropbox/NYU/Projects/SF/MyResults/',
-              save_file_name='model_pred.png'):
+                dp_to_x_axis='norm_betas', dp_to_y_axis='norm_pred',
+                x_axis_label='Measured Betas', y_axis_label="Model estimation",
+                legend_title="Eccentricity", labels=['~0.5°', '0.5-1°', '1-2°', '2-4°', '4+°'],
+                n_row=4, legend_out=True, alpha=0.5, bins=30, set_max=False,
+                save_fig=False, save_dir='/Users/auna/Dropbox/NYU/Projects/SF/MyResults/',
+                save_file_name='model_pred.png'):
     subj = utils.sub_number_to_string(sn)
     cur_df = df.query('subj == @subj')
     col_order = utils.sort_a_df_column(cur_df[to_subplot])
@@ -170,12 +173,13 @@ def beta_2Dhist(sn, df, to_subplot="vroinames", to_label="eccrois",
         plt.savefig(save_path)
     plt.show()
 
+
 def beta_1Dhist(sn, df, to_subplot="vroinames",
-              x_axis_label='Beta', y_axis_label="Probability",
-              legend_title="Beta type", labels=['measured betas', 'model prediction'],
-              n_row=4, legend_out=True, alpha=0.5, bins=30,
-              save_fig=False, save_dir='/Users/auna/Dropbox/NYU/Projects/SF/MyResults/',
-              save_file_name='model_pred.png'):
+                x_axis_label='Beta', y_axis_label="Probability",
+                legend_title="Beta type", labels=['measured betas', 'model prediction'],
+                n_row=4, legend_out=True, alpha=0.5, bins=30,
+                save_fig=False, save_dir='/Users/auna/Dropbox/NYU/Projects/SF/MyResults/',
+                save_file_name='model_pred.png'):
     subj = utils.sub_number_to_string(sn)
     cur_df = df.query('subj == @subj')
     melt_df = pd.melt(cur_df, id_vars=['subj', 'voxel', 'vroinames'], value_vars=['norm_betas', 'norm_pred'],
@@ -208,6 +212,7 @@ def beta_1Dhist(sn, df, to_subplot="vroinames",
         plt.savefig(save_path)
     plt.show()
 
+
 # numpy to torch function
 def _cast_as_tensor(x):
     """ Change numpy vector to torch vector. The input x should be either a column of dataframe,
@@ -217,12 +222,13 @@ def _cast_as_tensor(x):
     # needs to be float32 to work with the Hessian calculations
     return torch.tensor(x, dtype=torch.float32)
 
+
 def _cast_as_param(x, requires_grad=True):
     """ Change input x to """
     return torch.nn.Parameter(_cast_as_tensor(x), requires_grad=requires_grad)
 
-def _cast_args_as_tensors(args, on_cuda=False):
 
+def _cast_args_as_tensors(args, on_cuda=False):
     return_args = []
     for v in args:
         if not torch.is_tensor(v):
@@ -232,12 +238,15 @@ def _cast_args_as_tensors(args, on_cuda=False):
         return_args.append(v)
     return return_args
 
+
+def count_nan_in_torch_vector(x):
+    return torch.nonzero(torch.isnan(torch.log2(x).view(-1)))
+
+
 # SF model class
 class SpatialFrequencyModel(torch.nn.Module):
     def __init__(self):
-
-        super().__init__()
-
+        super().__init__()  # Allows us to avoid using the base class name explicitly
         self.sigma = _cast_as_param(np.random.random(1))
         self.slope = _cast_as_param(np.random.random(1))
         self.intercept = _cast_as_param(np.random.random(1))
@@ -249,7 +258,6 @@ class SpatialFrequencyModel(torch.nn.Module):
         self.A_2 = _cast_as_param(np.random.random(1))
         self.A_3 = 0
         self.A_4 = 0
-
 
     def get_Av(self, theta_l, theta_v):
         """ Calculate A_v (formula no. 7 in Broderick et al. (2022)) """
@@ -274,19 +282,40 @@ class SpatialFrequencyModel(torch.nn.Module):
         return Pv
 
     def forward(self, theta_l, theta_v, r_v, w_l):
-        """ Return predicted BOLD response in eccentricity (formula no. 5 in Broderick et al. (2022)) """
+        """
+                        In the forward function we accept a Variable of input data and we must
+                        return a Variable of output data. Return predicted BOLD response
+                        in eccentricity (formula no. 5 in Broderick et al. (2022))
+                        """
         w_l = _cast_as_tensor(w_l)
 
         Av = self.get_Av(theta_l, theta_v)
         Pv = self.get_Pv(theta_l, theta_v, r_v)
-        return Av * torch.exp(-(torch.log2(w_l) + torch.log2(Pv)) ** 2 / (2 * self.sigma ** 2))
+        pred = Av * torch.exp(-(torch.log2(w_l) + torch.log2(Pv)) ** 2 / (2 * self.sigma ** 2))
+        return pred
 
 
-    def objective_func(self, x, y):
-        mse_loss = torch.nn.functional.mse_loss(x, torch.from_numpy(y).to(torch.float32))
+def loss_fn(predictions, target):
+    return loss
 
-        #Lv = (1/self.sigma**2)
-        return mse_loss
 
-def count_nan_in_torch_vector(x):
-    return torch.nonzero(torch.isnan(torch.log2(x).view(-1)))
+def fit_model(model, dataset, learning_rate=1e-3, max_epoch=1000):
+    """Fit the model. This function will allow you to run a for loop for N times set as max_epoch,
+    and return the output of the training; loss history, model history."""
+
+    my_parameters = model.parameters()
+
+    optimizer = torch.adam.optimizer(my_parameters, lr=learning_rate)
+    loss_history = []
+    model_history = []
+    for t in range(max_epoch):
+        # predictions should be come out here
+        loss = loss_fn(predictions, target)  # loss should be returned here
+        # output needs to be put in there
+        loss_history[t].append(loss.item())
+        model_history[t].append()  # more than one item here
+
+        optimizer.zero_grad()  # clear previous gradients
+        loss.backward()  # compute gradients of all variables wrt loss
+        optimizer.step()  # perform updates using calculated gradients
+        model.eval()
