1fbafbd13c53311aff77536c452ac464555ddee1
Jiyeong Ha <jiyeongha@yonsei.ac.kr>
2022-06-15 17:41:18 -0700

    simulation functions into class form

--- simulation.py
+++ simulation.py
@@ -9,36 +9,52 @@ import make_df
 from  itertools import combinations
 
 
-def get_w_a_w_r(stim_description_path='/Users/jh7685/Dropbox/NYU/Projects/SF/natural-scenes-dataset/derivatives/nsdsynthetic_sf_stim_description.csv'):
-    stim_info = make_df._load_stim_info(stim_description_path=stim_description_path)
+def _get_stim_info(stim_info_path='/Users/jh7685/Dropbox/NYU/Projects/SF/natural-scenes-dataset/derivatives/nsdsynthetic_sf_stim_description.csv'):
+    """Load stim info.csv and remove phase information.
+    For each unique combination of stim classes and frequency levels, you will have one row.
+    This is mainly to get w_a and w_r for each stim type (the combinations)"""
+
+    stim_info = make_df._load_stim_info(stim_info_path)
     stim_info = pd.DataFrame(stim_info)
     stim_info = stim_info.query('phase_idx == 0')
     stim_info = stim_info.drop(columns=['phase', 'phase_idx'])
     return stim_info
 
-def replicate_df(stim_info, n_replicate=100):
-    stim_info['voxel'] = 0
-    tmp_df = stim_info.copy()
-    for i in np.arange(1, n_replicate):
-        tmp_df['voxel'] = i
-        stim_info = pd.concat([stim_info, tmp_df], ignore_index=True)
-    return stim_info
+class SynthesizeData():
+    """Synthesize data for 1D model and 2D model simulations. This class consists of three parts:
+    1. Load stimulus information (stim class, frequency level, w_a, w_r, etc) without phase information
+    2. Generate synthetic voxels. Eccentricity and polar angle will be drawn from the uniform distribution.
+    3. Generate BOLD predictions, with or without noise. """
 
-def generate_synthesized_data(n_voxels=100, stim_description_path='/Users/jh7685/Dropbox/NYU/Projects/SF/natural-scenes-dataset/derivatives/nsdsynthetic_sf_stim_description.csv'):
-    """Generate synthesized data for n voxels.
-    Each voxel's polar angle and eccentricity will be drawn from uniform distribution.
-    The polar angle is in the unit of degree and eccentricity is in the unit of visual angle."""
-    stim_info = get_w_a_w_r(stim_description_path)
-    stim_info = replicate_df(stim_info, n_replicate=n_voxels)
+    def __init__(self, n_voxels=100, stim_info_path='/Users/auna/Dropbox/NYU/Projects/SF/natural-scenes-dataset/derivatives/nsdsynthetic_sf_stim_description.csv'):
+        self.n_voxels = n_voxels
+        self.stim_info = self.get_stim_info_for_n_voxels(stim_info_path)
+        self.syn_df = self.generate_synthetic_voxels()
 
-    df = pd.DataFrame()
-    df['voxel'] = np.arange(0, n_voxels)
-    df['angle'] = np.random.uniform(0, 360, size=n_voxels)
-    df['eccentricity'] = np.random.uniform(0, 4.2, size=n_voxels)
-    syn_df = stim_info.merge(df, on='voxel')
-    syn_df = make_df._calculate_local_orientation(syn_df)
-    syn_df = make_df._calculate_local_sf(syn_df)
-    return syn_df
+    def get_stim_info_for_n_voxels(self, stim_info_path):
+        stim_info = _get_stim_info(stim_info_path)
+        stim_info['voxel'] = 0
+        tmp_df = stim_info.copy()
+        for i in np.arange(1, self.n_voxels):
+            tmp_df['voxel'] = i
+            stim_info = pd.concat([stim_info, tmp_df], ignore_index=True)
+        return stim_info
+
+    def generate_synthetic_voxels(self):
+        """Generate synthesized data for n voxels.
+        Each voxel's polar angle and eccentricity will be drawn from uniform distribution.
+        The polar angle is in the unit of degree and eccentricity is in the unit of visual angle."""
+        #TODO: add another distribution
+        df = pd.DataFrame()
+        df['voxel'] = np.arange(0, self.n_voxels)
+        df['angle'] = np.random.uniform(0, 360, size=self.n_voxels)
+        df['eccentricity'] = np.random.uniform(0, 4.2, size=self.n_voxels)
+        syn_df = self.stim_info.merge(df, on='voxel')
+        syn_df = make_df._calculate_local_orientation(syn_df)
+        syn_df = make_df._calculate_local_sf(syn_df)
+        return syn_df
+
+
 
 def add_noise(betas, noise_mean=0, noise_sd=0.05):
     return betas + np.random.normal(noise_mean, noise_sd, len(betas))
