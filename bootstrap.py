d7df3130587e9395bcc9704f04e8ae8bb74df73e
Jiyeong Ha <jiyeongha@yonsei.ac.kr>
2022-06-20 18:34:16 -0700

    bootstrap functions added

--- bootstrap.py
+++ bootstrap.py
@@ -7,6 +7,7 @@ import sfp_nsd_utils as utils
 import pandas as pd
 import numpy as np
 from first_level_analysis import np_log_norm_pdf
+from tqdm import tqdm
 
 def bootstrap_sample(data, stat=np.mean, n_select=8, n_bootstrap=100):
     """ Bootstrap sample from data"""
@@ -17,8 +18,42 @@ def bootstrap_sample(data, stat=np.mean, n_select=8, n_bootstrap=100):
         bootstrap.append(i_bootstrap)
     return bootstrap
 
-def get_all_trials_for_each_stim(df):
-    """ Get all trials for each stimulus"""
 
-    return trials
+def bootstrap_dataframe(df, n_bootstrap=100,
+                        to_sample='avg_betas',
+                        to_group=['voxel', 'names', 'freq_lvl'], replace=True):
+    """ Bootstrap using a dataframe. Progress bar will be displayed according to the
+    number of the voxels for each subject."""
 
+    selected_cols = to_group + [to_sample]
+    all_df = pd.DataFrame(columns=selected_cols)
+    for i_v in tqdm(df.voxel.unique()[0:3]):
+        sample_df = df.query('voxel == @i_v')
+        for i in range(n_bootstrap):
+            tmp = sample_df[selected_cols].groupby(to_group).sample(n=8, replace=replace)
+            tmp = sample_df.groupby(to_group).mean().reset_index()
+            tmp['bootstrap'] = i
+            tmp['bootstrap'] = tmp['bootstrap'].astype(int)
+            all_df = pd.concat([all_df, tmp], ignore_index=True)
+
+    return all_df
+
+def bootstrap_dataframe_all_subj(sn_list, df, n_bootstrap=100,
+                        to_sample='betas',
+                        to_group=['subj', 'voxel', 'names', 'freq_lvl'], replace=True):
+    """ Bootstrap for each subject's dataframe. Message will be displayed for each subject."""
+
+    selected_cols = to_group + [to_sample]
+    all_df = pd.DataFrame(columns=selected_cols)
+    for sn in sn_list:
+        subj = utils.sub_number_to_string(sn)
+        tmp = df.query('subj == @subj')
+        print(f'***{subj} bootstrapping start!***')
+        tmp = bootstrap_dataframe(tmp,
+                                  n_bootstrap=n_bootstrap,
+                                  to_sample=to_sample,
+                                  to_group=to_group,
+                                  replace=replace)
+        all_df = pd.concat([all_df, tmp], ignore_index=True)
+
+    return all_df
