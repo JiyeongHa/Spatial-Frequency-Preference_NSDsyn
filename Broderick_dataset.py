e0c456c63eb6218c3ac1a0d4a4e63eee43d7e471
Jiyeong Ha <jiyeong.ha@nyu.edu>
2022-07-26 16:05:10 -0400

    Broderick_dataset.py created

---
+++ Broderick_dataset.py
@@ -0,0 +1,75 @@
+import sys
+sys.path.append('/Users/jh7685/Documents/GitHub/spatial-frequency-preferences')
+import os
+import itertools
+import nibabel as nib
+import numpy as np
+import pandas as pd
+import h5py
+import itertools
+import pandas as pd
+from scipy.io import loadmat
+
+def _label_stim_names(row):
+    if row.w_r == 0 and row.w_a != 0:
+        return 'pinwheel'
+    elif row.w_r != 0 and row.w_a == 0:
+        return 'annulus'
+    elif row.w_r == row.w_a:
+        return 'forward spiral'
+    elif row.w_r == -1*row.w_a:
+        return 'reverse spiral'
+    else:
+        return 'mixtures'
+
+def _label_freq_lvl(freq_lvl=10, phi_repeat=8, main_classes=4, mixture_freq_lvl=5, n_mixtures=8):
+    freq_lvl_main_classes = np.tile(np.repeat(np.arange(0, freq_lvl), phi_repeat), main_classes)
+    freq_lvl_mixtures = np.repeat(mixture_freq_lvl, phi_repeat * n_mixtures)
+    freq_lvl = np.concatenate((freq_lvl_main_classes, freq_lvl_mixtures))
+    return freq_lvl
+
+def load_stim_info(stim_description_path='/Volumes/server/Projects/sfp_nsd/Broderick_dataset/stimuli/task-sfprescaled_stim_description_haji.csv',
+                   save_copy=True):
+    """stimulus description file will be loaded as a dataframe."""
+
+    # stimuli information
+    if os.path.exists(stim_description_path):
+        stim_df = pd.read_csv(stim_description_path)
+    else:
+        if not os.path.exists(stim_description_path.replace('_haji.csv', '.csv')):
+            raise Exception('The original stim description file does not exist!\n')
+        stim_df = pd.read_csv(stim_description_path)
+        stim_df = stim_df.dropna(axis=0)
+        stim_df = stim_df.astype({'class_idx': int})
+        stim_df = stim_df.drop(columns='res')
+        stim_df = stim_df.rename(columns={'phi': 'phase', 'index': 'image_idx'})
+        stim_df['names'] = stim_df.apply(_label_stim_names, axis=1)
+        stim_df['freq_lvl'] = _label_freq_lvl()
+        stim_df.to_csv(stim_description_path, index=False)
+    return stim_df
+
+
+def _get_benson_atlas_rois(roi_num):
+    # load GLMdenoise file
+    # f.keys() -> shows betas
+    num_key = {1: "V1", 2: "V2", 3: "V3", 4: "hV4", 5: "VO1", 6: "VO2",
+        7: "LO1", 8: "LO2", 9: "TO1", 10: "TO2", 11: "V3b", 12: "V3a"}
+    name_key = {y: x for x, y in num_key.items()}
+    switcher = {**num_key, **name_key}
+    return switcher.get(roi_num, "No Visual area")
+
+
+def load_roi(sn, prf_label_names=['angle','eccen','sigma']):
+    """ Load inferred_varea.mgz for each hemisphere and each subject. see https://osf.io/knb5g/wiki/Usage/"""
+    for hemi, prf_names in itertools.product(['lh','rh'], prf_label_names):
+        varea_name = f"{hemi}.inferred_varea.mgz"
+        varea_path = os.path.join('/Volumes/server/Projects/sfp_nsd/Broderick_dataset/derivatives/prf_solutions/', "sub-wlsubj{:03d}".format(sn), 'bayesian_posterior', varea_name)
+        varea = nib.load(varea_path).get_fdata().squeeze()
+    return prf
+
+def load_prf(sn, prf_label_names=['angle','eccen','sigma']):
+    for hemi, prf_names in itertools.product(['lh','rh'], prf_label_names):
+        prf_name = f"{hemi}.inferred_{prf_label_names}.mgz"
+        prf_path = os.path.join('/Volumes/server/Projects/sfp_nsd/Broderick_dataset/derivatives/prf_solutions/', "sub-wlsubj{:03d}".format(sn), 'bayesian_posterior', prf_name)
+        prf = nib.load(prf_path).get_fdata().squeeze()
+    return prf
