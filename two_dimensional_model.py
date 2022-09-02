d0376f7e8fb8a25f61be58074fa7e22fa149a7d4
Jiyeong Ha <jiyeong.ha@nyu.edu>
2022-09-02 18:58:36 -0400

    deleted class PredictBOLD2d()

--- two_dimensional_model.py
+++ two_dimensional_model.py
@@ -25,59 +25,6 @@ def break_down_phase(df):
 
     return df
 
-
-class PredictBOLD2d():
-    """ Define parameters used in forward model"""
-
-    def __init__(self, params, params_idx, subj_df):
-        self.params_df = params.iloc[params_idx]
-        self.sigma = self.params_df['sigma']
-        self.amp = self.params_df['slope']
-        self.intercept = self.params_df['intercept']
-        self.p_1 = self.params_df['p_1']
-        self.p_2 = self.params_df['p_2']
-        self.p_3 = self.params_df['p_3']
-        self.p_4 = self.params_df['p_4']
-        self.A_1 = self.params_df['A_1']
-        self.A_2 = self.params_df['A_2']
-        self.A_3 = self.params_df['A_3']
-        self.A_4 = self.params_df['A_4']
-        self.subj_df = subj_df.copy()
-        self.theta_l = self.subj_df['local_ori']
-        self.theta_v = self.subj_df['angle']
-        self.r_v = self.subj_df['eccentricity']  # voxel eccentricity (in degrees)
-        self.w_l = self.subj_df['local_sf']  # in cycles per degree
-
-    def get_Av(self, full_ver):
-        """ Calculate A_v (formula no. 7 in Broderick et al. (2022)) """
-        if full_ver is True:
-            Av = 1 + self.A_1 * np.cos(2 * self.theta_l) + \
-                 self.A_2 * np.cos(4 * self.theta_l) + \
-                 self.A_3 * np.cos(2 * (self.theta_l - self.theta_v)) + \
-                 self.A_4 * np.cos(4 * (self.theta_l - self.theta_v))
-        elif full_ver is False:
-            Av = 1
-        return Av
-
-    def get_Pv(self, full_ver):
-        """ Calculate p_v (formula no. 6 in Broderick et al. (2022)) """
-        ecc_dependency = self.amp * self.r_v + self.intercept
-        if full_ver is True:
-            Pv = ecc_dependency * (1 + self.p_1 * np.cos(2 * self.theta_l) +
-                                   self.p_2 * np.cos(4 * self.theta_l) +
-                                   self.p_3 * np.cos(2 * (self.theta_l - self.theta_v)) +
-                                   self.p_4 * np.cos(4 * (self.theta_l - self.theta_v)))
-        elif full_ver is False:
-            Pv = ecc_dependency
-        return Pv
-
-    def forward(self, full_ver=True):
-        """ Return predicted BOLD response in eccentricity (formula no. 5 in Broderick et al. (2022)) """
-        Av = self.get_Av(full_ver=full_ver)
-        Pv = self.get_Pv(full_ver=full_ver)
-        return Av * np.exp(-(np.log2(self.w_l) + np.log2(Pv)) ** 2 / (2 * self.sigma ** 2))
-
-
 def normalize(voxel_info, to_norm, to_group=["subj", "voxel"], phase_info=False):
     """calculate L2 norm for each voxel and normalized using the L2 norm"""
 
